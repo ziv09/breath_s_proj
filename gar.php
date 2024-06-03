bbbddc66eddaa7b520f6dd1d2d6a0490f3cda8de
ziv09 <wfs.yang9050@gmail.com>
2024-06-03 16:12:35 +0800

    0603新頁面1

---
+++ gar.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>生活呼吸</title>
+    <link href="./css/main_sy.css" rel="stylesheet">
+    <link href="./css/gar_sy.css" rel="stylesheet">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap" rel="stylesheet">
+</head>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        var currentDate = new Date();
+        var options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+        var formattedDate = currentDate.toLocaleDateString("zh-Hant", options);
+        document.getElementById("current-date").innerText = formattedDate;
+    });
+</script>
+
+<body class="body">
+    <div class="barcon">
+        <img src="./images/logo.png" alt="">
+        <div>
+            <nav>
+                <ul class="barstyle">
+                    <li><a href="./index.php" id="bar">Home</a></li>
+                    <li><a href="./login.php" id="bar">Account</a></li>
+                    <li><a href="./amount.php" id="bar">Money</a></li>
+                    <li><a href="./map.php" id="bar">Map</a></li>
+                    <li><a href="./about.php" id="bar">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="date">
+        每日碳排&ensp;|&ensp;<span id="current-date"></span>
+    </div>
+    <div>
+        <div class="co_record">
+            <table class="table">
+                <tr>
+                    <td>環保餐盒<select class="select" name="numbers">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select></td>
+                        <td>環保筷<select class="select" name="numbers">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select></td>
+                        <td>環保杯<select class="select" name="numbers">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select></td>
+                </tr>
+                <tr>
+                <td>環保袋<select class="select" name="numbers">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select></td>
+                        <td>環保吸管<select class="select" name="numbers">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select></td>
+                        <td>大眾運輸<select class="select" name="numbers">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select></td>
+                </tr>
+
+        </div>
+    </div>
+
+
+    </div>
+
+
+
+</body>
+
+</html>
